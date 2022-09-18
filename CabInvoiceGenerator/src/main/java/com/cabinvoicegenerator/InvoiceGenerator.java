f0c0da725db831df50ec2f54cdb79fc119b219b6
Veer1498 <singavarapuveerendra@gmail.com>
2022-09-19 02:58:47 +0530

    [Veer] Added : Ability to check fare when multiple rides happening

--- CabInvoiceGenerator/src/main/java/com/cabinvoicegenerator/InvoiceGenerator.java
+++ CabInvoiceGenerator/src/main/java/com/cabinvoicegenerator/InvoiceGenerator.java
@@ -5,7 +5,7 @@ public class InvoiceGenerator {
     public static final int COST_PER_MIN = 1;
     public static final int MIN_FARE = 5;
 
-
+    //Function to Calculate cab Fare.
     public double fareCalculation(double distance, double time){
         double fare;
         if (distance<(0.5)){
@@ -14,5 +14,14 @@ public class InvoiceGenerator {
         }
             fare =  (distance*COST_PER_KM)+(time*COST_PER_MIN);
         return fare;
+    }
+    //Functions to calculate for multiple rides
+    public double calculateFare(RideDetails[] rides) {
+        double totalFare = 0;
+        for (RideDetails ride : rides) {
+
+            totalFare += this.fareCalculation(ride.getDistance(), ride.getTime());
+        }
+        return totalFare;
     }
 }
--- CabInvoiceGenerator/src/test/java/com/cabinvoicegenerator/InvoiceGeneratorTest.java
+++ CabInvoiceGenerator/src/test/java/com/cabinvoicegenerator/InvoiceGeneratorTest.java
@@ -1,5 +1,37 @@
 package com.cabinvoicegenerator;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class InvoiceGeneratorTest {
+
+    @Test
+    public void passingDistAndTimeShldReturn_CrctFare(){
+        double distance = 3;
+        double time = 10;
+        InvoiceGenerator generator = new InvoiceGenerator();
+        double result = generator.fareCalculation(distance,time);
+        Assertions.assertEquals(40.0,result);
+    }
+
+    @Test
+    public void passingMinDistAndTimeShldReturn_CrctFare(){
+        double distance = 0.2;
+        double time = 1;
+        InvoiceGenerator generator = new InvoiceGenerator();
+        double result = generator.fareCalculation(distance,time);
+        Assertions.assertEquals(5.0,result);
+    }
+
+    @Test
+    public void passingMultipleRidesShldReturn_CrctFare(){
+       RideDetails[] ride = {
+               new RideDetails(2.0,4),
+               new RideDetails(0.1,1)
+                };
+        InvoiceGenerator generator = new InvoiceGenerator();
+        double result = generator.calculateFare(ride);
+        Assertions.assertEquals(29.0,result);
+    }
 
 }
